6ba4c7aa9afd6443f2ac55e5eb666a3598ab76fd
haenter <hossein.amiri.enterprise@gmail.com>
2022-05-03 15:17:59 +0430

    Scroll Disabled Conditionally

--- pages/scrollImage.js
+++ pages/scrollImage.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/ScrollImage.module.css";
 const ScrollImage = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -13,22 +13,26 @@ const ScrollImage = () => {
     "/images/image7.png",
   ];
   const onWheelHandler = (e) => {
+    document.body.style.overflow = "hidden";
     if (e.deltaY < 0) {
       console.log("up");
       currentImage > 1 && setCurrentImage(currentImage - 1);
     } else if (e.deltaY > 0) {
       console.log("down");
-      currentImage < image.length - 1 && setCurrentImage(currentImage + 1);
+      currentImage < image.length - 1
+        ? setCurrentImage(currentImage + 1)
+        : (document.body.style.overflow = "");
     }
   };
+
   return (
     <div>
       <div>
         <Image
           onWheel={onWheelHandler}
           className={styles.image}
-          width={300}
-          height={300}
+          width={4300}
+          height={4300}
           src={image[currentImage]}
         ></Image>
       </div>
